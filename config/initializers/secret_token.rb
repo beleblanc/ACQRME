142862d71b439088d7df8437c87acd9c7b21cfd6
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-04-04 11:46:37 +0200

    Initial commit to create repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ACQRME::Application.config.secret_token = '0b10b84828bbe8a7f6853ac4d0c96c829dd9aeae1f1e790a309476256a5eea2e2821619b88c948626765f1b965262bff06668dede9375b25d2a1dcde3c5b4bdd'
