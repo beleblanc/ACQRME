cccad50e994a275cd9234bcd1fb6409b3bce592d
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-04-10 17:21:25 +0200

    added authentication layout partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
       puts "<p>Signed in as #{current_user.email}</p>"
       link_to "Sign Out", destroy_user_session_path, :method => :delete
     else
-       puts link_to "Sign up", new_user_registration_path
+        link_to "Sign up", new_user_registration_path
        link_to "Sign in", new_user_session_path
     end
   end
